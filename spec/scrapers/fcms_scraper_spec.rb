bb4f97e0ba2733a83bd5d5ebbea0b76c6f32c0ed
Arek Mano <manoukian.arek@gmail.com>
2016-09-23 12:35:16 -0400

    more tests

--- spec/scrapers/fcms_scraper_spec.rb
+++ spec/scrapers/fcms_scraper_spec.rb
@@ -29,4 +29,58 @@ RSpec.describe FcmsScraper do
       expect(time.min).to eq(15)
     end
   end
+
+  describe 'course info' do
+    before(:each) do
+      @text = ['COMP 232', 'Computer Class']
+    end
+
+    describe 'course code parsing' do
+      it 'parses regular things correctly' do
+        scraper = FcmsScraper.new
+
+        result = scraper.course_code(@text)
+
+        expect(result).to eq('COMP')
+      end
+    end
+
+    describe 'course number parsing' do
+      it 'parses regular things correctly' do
+        scraper = FcmsScraper.new
+
+        result = scraper.course_number(@text)
+
+        expect(result).to eq('232')
+      end
+    end
+
+    describe 'course title parsing' do
+      it 'parses regular things correctly' do
+        scraper = FcmsScraper.new
+
+        result = scraper.course_name(@text)
+
+        expect(result).to eq('Computer Class')
+      end
+    end
+  end
+
+  describe 'section type parsing' do
+    it 'parses normal things correctly' do
+      scraper = FcmsScraper.new
+
+      result = scraper.parse_section_type('WK13-LAB-FSG')
+
+      expect(result).to eq('LAB')
+    end
+
+    it 'returns nil when section type is unrecognized' do
+      scraper = FcmsScraper.new
+
+      result = scraper.parse_section_type('WK13-BOA-FSG')
+
+      expect(result).to be_nil
+    end
+  end
 end
