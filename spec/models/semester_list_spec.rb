171b32c8cfc5fc690d9053ce0925167e4765ecef
Arek Mano <manoukian.arek@gmail.com>
2016-09-23 12:35:16 -0400

    more tests

---
+++ spec/models/semester_list_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../spec_helper'
+require_relative '../../lib/models/semester_list'
+require_relative '../../lib/models/semester'
+
+RSpec.describe SemesterList do
+  describe 'initialization' do
+
+  end
+end
