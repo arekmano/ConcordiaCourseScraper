ec54f28e41ef481e3f2d9bb061c38e6abe11bf57
= <=>
2016-09-28 18:57:48 -0400

    entered DB info in environment

--- run.rb
+++ run.rb
@@ -1,5 +1,18 @@
 require_relative './lib/concordia_course_scraper'
+require_relative './lib/database_populators/sql_populator'
 
-scraper = ConcordiaCourseScraper.new(course_codes: 'ALL')
-scraper.extract_all
+populator = SqlPopulator.new(
+  username: ENV['CONCORDIA_COURSE_USER'],
+  password: ENV['CONCORDIA_COURSE_PASS'],
+  host: ENV['CONCORDIA_COURSE_HOST'],
+  database: ENV['CONCORDIA_COURSE_DB']
+)
+scraper = ConcordiaCourseScraper.new(
+    course_codes: 'ALL',
+    database_populator: populator
+)
+scraper.extract_all(214)
+scraper.extract_all(215)
+scraper.extract_all(216)
+
 scraper.save
